532cef902a09893d705c79fbb9dc7bb0675e5273
dipeshio <156320602+dipeshio@users.noreply.github.com>
2025-02-03 12:46:26 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// create a function that takes the current web page and extracts the name of assignments, and dates due,
+// and returns an array of objects with the name and date of each assignment.
+
+// Example: Homework: Week 3 due Wednesday (HW 3W) -> [{name: "Homework: Week 3", date: "02/05/2025", time: "11:59 PM"}]
+// function should handle multiple assignments at once.
+
